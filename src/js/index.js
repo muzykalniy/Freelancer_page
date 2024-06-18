6eb713568326839be79496798ac8690c3e6c25f3
Andrey Muzyka <muzykalniy@MacBook-Air-muzykalniy.local>
2024-06-18 12:28:56 +0200

    commit swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,24 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// // Галерея и лайтбоксы от Fancybox
+// import { Fancybox } from '@fancyapps/ui';
+// import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// Fancybox.bind('[data-fancybox]', {
+// 	// Your custom options
+// });
+
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+// init Portfolio-Swiper:
+var swiper = new Swiper(".portfolioSwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
 });
-
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
